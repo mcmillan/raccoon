5c04d71b27a54e329879d66590ca941ba1281325
Josh McMillan <josh@joshmcmillan.com>
2022-06-21 15:51:40 +0100

    feat(server): add blank Mix application

---
+++ server/lib/raccoon.ex
@@ -0,0 +1,2 @@
+defmodule Raccoon do
+end
---
+++ server/lib/raccoon/application.ex
@@ -0,0 +1,11 @@
+defmodule Raccoon.Application do
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = []
+
+    opts = [strategy: :one_for_one, name: Raccoon.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
