a9d995451852d21f06521cb266ad34d66c2e5578
Josh McMillan <josh@joshmcmillan.com>
2022-06-22 17:43:02 +0100

    feat(server): include an `updated_at` field alongside data

--- server/lib/raccoon/store.ex
+++ server/lib/raccoon/store.ex
@@ -1,15 +1,24 @@
 defmodule Raccoon.Store do
   def get do
-    case Redix.command(:redis, ["GET", "raccoon_data"]) do
-      {:ok, nil} ->
+    case Redix.command(:redis, ["MGET", "raccoon_data", "raccoon_updated_at"]) do
+      {:ok, [nil, nil]} ->
         []
 
-      {:ok, data} ->
-        Jason.decode!(data)
+      {:ok, [data, updated_at]} ->
+        %{data: Jason.decode!(data), meta: %{updated_at: updated_at}}
     end
   end
 
   def set(data) do
-    Redix.command(:redis, ["SET", "raccoon_data", Jason.encode!(data)])
+    Redix.command(
+      :redis,
+      [
+        "MSET",
+        "raccoon_data",
+        Jason.encode!(data),
+        "raccoon_updated_at",
+        DateTime.utc_now() |> DateTime.to_iso8601()
+      ]
+    )
   end
 end
