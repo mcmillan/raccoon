274cdd849622066b9d554f2b2042ca1ade1adc20
Josh McMillan <josh@joshmcmillan.com>
2022-06-25 13:26:53 +0100

    feat(frame): add Inky integration

--- frame/frame.py
+++ frame/frame.py
@@ -1,8 +1,11 @@
 import api
 from renderer import render_error, render_collections
 from models import Collections
+from inky.auto import auto as inky
 
 if __name__ == "__main__":
+    display = inky()
+
     try:
         data, meta = api.get_collections()
         collections = Collections.from_dicts(data, meta["updated_at"])
@@ -10,4 +13,6 @@ if __name__ == "__main__":
     except BaseException as error:
         image = render_error(error)
 
-    image.show()
+    display.set_image(image)
+
+    display.show()
